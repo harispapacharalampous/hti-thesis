63b0377e308c91e92dab252019ced4c55770e680
haris <harispapacharalampous@gmail.com>
2020-05-28 15:07:39 +0200

    added meeting model and styled plans

---
+++ db/migrate/20200528130713_create_meetings.rb
@@ -0,0 +1,12 @@
+class CreateMeetings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :meetings do |t|
+      t.string :name
+      t.string :type
+      t.datetime :start_time
+      t.datetime :end_time
+
+      t.timestamps
+    end
+  end
+end
