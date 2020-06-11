17c2f0b72776ba8397d4b1e547af644c6bb244eb
haris <harispapacharalampous@gmail.com>
2020-06-11 17:38:01 +0200

    time zone added

--- config/application.rb
+++ config/application.rb
@@ -16,4 +16,7 @@ module UserFrame
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
   end
+
+config.time_zone = 'Amsterdam'
+
 end
