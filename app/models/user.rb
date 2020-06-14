1650357a105354a31a07a8416c241e04386c931b
haris <harispapacharalampous@gmail.com>
2020-06-14 15:19:41 +0200

    filtering of events and bult creation of two types of activities

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     has_many :posts
     has_many :meetings
+    #callback cue
     after_create :populate_meetings
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -12,18 +13,16 @@ class User < ApplicationRecord
     end
 
     private
-
+        #callback cue'd function
         def populate_meetings
             d = DateTime.now
-            #dates =Array.new
-            #dates.push(d)
-            #dates.push(d + 1.day)
-            #dates.push(d + 2.day)
-            #dates.push(d + 3.day)
-            #dates.push(d + 4.day)
-
+            #generate 5 events of  1h duration in 5 consecutive days type 1 (run)
             for b in 0..4 do
-                self.meetings.create( name: 'auto ' + b.to_s, activitytype: '420', start_time: (d + b.day ).strftime("%Y-%m-%d %H:%M"), end_time: (d + b.day + 1.hour).strftime("%Y-%m-%d %H:%M"), user_id: self);
+                self.meetings.create( name: 'run ' + b.to_s, activitytype: '1', start_time: (d + b.day ).strftime("%Y-%m-%d %H:%M"), end_time: (d + b.day + 1.hour).strftime("%Y-%m-%d %H:%M"), user_id: self);
+            end
+            #generate 5 events of  1h duration in 5 consecutive days type 2 (commute)
+            for c in 0..4 do
+                self.meetings.create( name: 'commute ' + c.to_s, activitytype: '2', start_time: (d + c.day + 2.hour ).strftime("%Y-%m-%d %H:%M"), end_time: (d + c.day + 3.hour).strftime("%Y-%m-%d %H:%M"), user_id: self);
             end
 
         end
