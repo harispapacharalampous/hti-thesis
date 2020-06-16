5d87dff2dfd693ef09ea9ad7cc547b4497553e6a
haris <harispapacharalampous@gmail.com>
2020-06-16 13:57:58 +0200

    time zone set up

--- app/models/coffee.rb
+++ app/models/coffee.rb
@@ -1,3 +1,5 @@
 class Coffee < ApplicationRecord
     belongs_to :user
+    #time zones
+    config.time_zone = 'Amsterdam'
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
     after_create :populate_meetings
     #callback cue
 
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
