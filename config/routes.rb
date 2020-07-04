6ec67dac4f6affc91406eb061d7f0907f12151c3
haris <harispapacharalampous@gmail.com>
2020-07-04 19:09:16 +0300

    added the new entry pre face

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :meetings
   resources :events
   resources :posts
-  root to: 'pages#home'
+  root to: 'posts#index'
   get "/pages/*page" => "pages#show"
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
