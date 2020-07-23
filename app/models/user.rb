134f37a838fa5cc2e3e331698be0c7f2b8cb60bd
haris <harispapacharalampous@gmail.com>
2020-07-23 15:41:32 +0300

    test github

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,7 +18,7 @@ class User < ApplicationRecord
 
     def after_database_authentication
         if self.condition ==1
-            if self.posts.count>=2
+            if self.posts.count>=3
                 RegMailer.comp_email(self).deliver_now
             end
         end
